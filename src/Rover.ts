765bd9b5badb222f414a761d8d03ef4dd6aad35e
Tommaso Resti <tommaso.resti@skyscanner.net>
2018-11-15 20:51:05 +0100

    Remove direction variable

--- src/Rover.ts
+++ src/Rover.ts
@@ -6,12 +6,10 @@ const directionObjToString: Map<Direction, string> = new Map([[NORTH, 'N'], [EAS
 export class Rover {
   private x: number;
   private y: number;
-  private direction: string;
   private directionObject: Direction;
   constructor(x: number, y: number, direction: string) {
     this.x = x;
     this.y = y;
-    this.direction = direction;
     this.directionObject = directionStringToObj.get(direction) || NORTH;
   }
 
@@ -19,12 +17,12 @@ export class Rover {
     [...commands].forEach(command => {
       if (command === 'L') {
         this.directionObject = this.directionObject.left();
-        this.direction = directionObjToString.get(this.directionObject) || 'N';
       } else {
         this.directionObject = this.directionObject.right();
-        this.direction = directionObjToString.get(this.directionObject) || 'N';
       }
     });
-    return `0:0:${this.direction}`;
+
+    const directionString = directionObjToString.get(this.directionObject) || 'N';
+    return `0:0:${directionString}`;
   }
 }
