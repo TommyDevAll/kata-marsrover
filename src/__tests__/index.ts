3b5be7ae26a54856b8e5f460e8a4e32136301bbe
Tommaso Resti <tommaso.resti@skyscanner.net>
2018-11-15 16:35:45 +0100

    turn left and right

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,7 +1,47 @@
-import { HelloWorld } from '../index';
+import { Rover } from '../index';
 
-describe('HelloWorld should', () => {
-  it('print a hello message', () => {
-    expect(new HelloWorld('Otto').getMessage()).toBe('Hello, this is Otto');
+describe('Rover', () => {
+  let rover: Rover;
+
+  describe('when facing NORTH', () => {
+    beforeEach(() => {
+      rover = new Rover(0, 0, 'N');
+    });
+
+    describe('when LEFT command is passed', () => {
+      it('once', () => {
+        expect(rover.move('L')).toBe('0:0:W');
+      });
+
+      it('twice', () => {
+        expect(rover.move('LL')).toBe('0:0:S');
+      });
+
+      it('tree times', () => {
+        expect(rover.move('LLL')).toBe('0:0:E');
+      });
+
+      it('four times', () => {
+        expect(rover.move('LLLL')).toBe('0:0:N');
+      });
+    });
+
+    describe('when RIGHT command is passed', () => {
+      it('once', () => {
+        expect(rover.move('R')).toBe('0:0:E');
+      });
+
+      it('twice', () => {
+        expect(rover.move('RR')).toBe('0:0:S');
+      });
+
+      it('tree times', () => {
+        expect(rover.move('RRR')).toBe('0:0:W');
+      });
+
+      it('four times', () => {
+        expect(rover.move('RRRR')).toBe('0:0:N');
+      });
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-export class HelloWorld {
-  readonly foo: string;
-
-  constructor(foo: string) {
-    this.foo = foo;
+export class Rover {
+  private x: number;
+  private y: number;
+  private direction: string;
+  constructor(x: number, y: number, direction: string) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
   }
 
-  getMessage() {
-    return `Hello, this is ${this.foo}`;
+  move(commands: string) {
+    [...commands].forEach(command => {
+      if (command === 'L') {
+        if (this.direction === 'N') {
+          this.direction = 'W';
+        } else if (this.direction === 'W') {
+          this.direction = 'S';
+        } else if (this.direction === 'S') {
+          this.direction = 'E';
+        } else {
+          this.direction = 'N';
+        }
+      } else {
+        if (this.direction === 'N') {
+          this.direction = 'E';
+        } else if (this.direction === 'E') {
+          this.direction = 'S';
+        } else if (this.direction === 'S') {
+          this.direction = 'W';
+        } else {
+          this.direction = 'N';
+        }
+      }
+    });
+    return `0:0:${this.direction}`;
   }
 }
