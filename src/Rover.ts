3b1128a4c7bd354cfa325a40ed09c24289c6b658
Tommaso Resti <tommaso.resti@skyscanner.net>
2018-11-15 20:58:11 +0100

    Refactoring

--- src/Rover.ts
+++ src/Rover.ts
@@ -3,26 +3,35 @@ import { Direction, EAST, NORTH, SOUTH, WEST } from './Direction';
 const directionStringToObj: Map<string, Direction> = new Map([['N', NORTH], ['E', EAST], ['W', WEST], ['S', SOUTH]]);
 const directionObjToString: Map<Direction, string> = new Map([[NORTH, 'N'], [EAST, 'E'], [WEST, 'W'], [SOUTH, 'S']]);
 
+interface Coordinates {
+  x: number;
+  y: number;
+}
+
+interface State {
+  coordinates: Coordinates;
+  direction: Direction;
+}
+
 export class Rover {
-  private x: number;
-  private y: number;
-  private directionObject: Direction;
+  private state: State;
   constructor(x: number, y: number, direction: string) {
-    this.x = x;
-    this.y = y;
-    this.directionObject = directionStringToObj.get(direction) || NORTH;
+    this.state = {
+      coordinates: { x, y },
+      direction: directionStringToObj.get(direction) || NORTH,
+    };
   }
 
   move(commands: string) {
     [...commands].forEach(command => {
       if (command === 'L') {
-        this.directionObject = this.directionObject.left();
+        this.state.direction = this.state.direction.left();
       } else {
-        this.directionObject = this.directionObject.right();
+        this.state.direction = this.state.direction.right();
       }
     });
 
-    const directionString = directionObjToString.get(this.directionObject) || 'N';
+    const directionString = directionObjToString.get(this.state.direction) || 'N';
     return `0:0:${directionString}`;
   }
 }
