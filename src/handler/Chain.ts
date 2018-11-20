83958e1f651bc594532ae82ecc4d0a55643aafa9
Tommaso Resti <tommaso.resti@skyscanner.net>
2018-11-20 01:36:16 +0100

    generic sameIdentifier and chain

--- src/handler/Chain.ts
+++ src/handler/Chain.ts
@@ -1,7 +1,7 @@
-import { RobotState, RobotStateHandler } from '../state/RobotState';
+import { Identifiable, StateHandler } from '../state/State';
 
-export const chain = (handlers: RobotStateHandler[]) => (state: RobotState) => {
-  let temporaryState: RobotState = state.update({});
+export const chain = <S extends Identifiable>(handlers: Array<StateHandler<S>>) => (state: S) => {
+  let temporaryState: S = state;
   handlers.forEach(handler => (temporaryState = handler(temporaryState)));
   return temporaryState;
 };
--- src/handler/SameIdentifier.ts
+++ src/handler/SameIdentifier.ts
@@ -1,6 +1,6 @@
 import { Identifiable, StateHandler } from '../state/State';
 
-export const sameIdentifier = <T, S extends Identifiable>(handlers: Array<StateHandler<S>>) => (state: S) => {
+export const sameIdentifier = <S extends Identifiable>(handlers: Array<StateHandler<S>>) => (state: S) => {
   let temporaryState: S = state;
   handlers.every(handler => {
     const condition = temporaryState.identifier;
